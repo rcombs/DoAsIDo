61a69ff3889ad902f8eed51b61938f0aa20a72a4
Rodger Combs <rodger.combs@gmail.com>
2012-08-21 17:34:45 -0500

    Initial commit

---
+++ public/gestures.js
@@ -0,0 +1,9 @@
+// TODO: WRITE FILE
+// Assuming that the element in question is a top-level one that can be moved anywhere in the window,
+// set it to be draggable to anywhere.
+function addDragMove(element){
+	
+}
+
+// Only works on elements with dragMove already added.
+// 
